11f9a370a9abb70f6dbb43240cb35f2d68d3df90
purpleundulations <purpleundulations@mail.com>
2015-02-11 14:48:45 -0600

    modified api, filters_controller and removed filters/show.slim

--- api.rb
+++
@@ -1,5 +0,0 @@
-class Api
-  def self.cases
-    ACCESS_TOKEN.get("#{API}/cases").body
-  end
-end
--- app/controllers/filters_controller.rb
+++ app/controllers/filters_controller.rb
@@ -2,9 +2,6 @@ class FiltersController < ApplicationController
   def index
     @list = Api::Filters.all
   end
-  def show
-    @filter = Api::Filters.select(params[:id])
-  end
   def cases
     @cases = Api::Filters.cases(params[:filter_id])
     @labels = Api::Labels.all
